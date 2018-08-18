0a1ecb8d6c64adad92d0dfc7efbca31d9b1ef1db
autopp <autopp.inc@gmail.com>
2018-08-18 17:14:14 +0900

    feat: impl filtering by timestamp of event

--- lib/fluent/plugin/filter_time.rb
+++ lib/fluent/plugin/filter_time.rb
@@ -5,5 +5,14 @@ module Fluent
   #
   class Plugin::TimeFilter < Plugin::Filter
     Fluent::Plugin.register_filter('time', self)
+
+    config_param :threshold, :integer, desc: <<~EODESC
+      Threshold for filtering by timestamp.
+      Events whose timestamp is older than `threshold` seconds ago are filtered out.
+    EODESC
+
+    def filter(_tag, time, record)
+      record if time >= Fluent::Engine.now - threshold
+    end
   end
 end
