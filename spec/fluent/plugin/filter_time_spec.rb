00ba478d0383b7353a9462d5a04254f2245504eb
autopp <autopp.inc@gmail.com>
2018-08-18 17:01:32 +0900

    test: add test case for #filter

--- spec/fluent/plugin/filter_time_spec.rb
+++ spec/fluent/plugin/filter_time_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
+require 'timecop'
+require 'fluent/test'
+require 'fluent/test/filter_test'
 
 describe Fluent::Plugin::TimeFilter do
+  before { Fluent::Test.setup }
+
+  let(:d) do
+    conf = <<~EOCONF
+      threshold 60
+    EOCONF
+    Fluent::Test::FilterTestDriver.new(described_class, 'foo').configure(conf)
+  end
+
+  describe '#filter' do
+    let(:now) { 1_500_000_000 }
+
+    before { Timecop.freeze(Time.at(now)) }
+    after { Timecop.return }
+
+    it 'filters records older than threshold' do
+      d.run do
+        d.emit({ number: 1 }, now - 61)
+        d.emit({ number: 2 }, now - 60)
+        d.emit({ number: 3 }, now - 59)
+      end
+
+      expect(d.emits).to eq([['foo', now - 60, { number: 2 }], ['foo', now - 59, { number: 3 }]])
+    end
+  end
 end
